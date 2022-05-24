eea7b1480e5ecf23efadb3af581e88b353f0f875
privet <810336311@qq.com>
2022-05-24 17:20:10 +0800

    生命周期

---
+++ vue/vue2/第5章 生命周期与数据共享/life_cycle/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <button @click="flag=!flag">Toggle Flag</button>
+    <Test info="你好" v-if="flag"></Test>
+  </div>
+</template>
+
+<script>
+import Test from '@/components/Test.vue';
+
+export default {
+  data() {
+    return {
+      flag:true
+    }
+  },
+  components: {
+    Test
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue2/第5章 生命周期与数据共享/life_cycle/src/components/Test.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="test-container">
+    <h3 id="myh3">Test.vue 组件</h3>
+    <p id="pp">message 的值是：{{ message }}</p>
+    <button @click="message += '~'">修改message的值</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['info'],
+  data(){
+    return {
+      message:'hello vue'
+    }
+  },
+  methods: {
+    show(){
+      console.log('调用了 Test 组件的 show 方法！');
+    }
+  },
+  // 创建阶段的第一个生命周期函数
+  beforeCreate() {
+    // console.log(this.info);
+    // console.log(this.message);
+    // this.show();
+  },
+  // 
+  created() {
+    // console.log(this.info);
+    // console.log(this.message);
+    // this.show();
+    // const dom=document.querySelector('#myh3');
+    // console.log(dom);
+  },
+  beforeMount() {
+    // const dom=document.querySelector('#myh3');
+    // console.log(dom);
+  },
+  mounted() {
+    // const dom=document.querySelector('#myh3');
+    // console.log(dom);
+  },
+  beforeUpdate() {
+    // console.log('beforeUpdate');
+    // const dom=document.querySelector('#pp');
+    // console.log(dom.innerHTML);
+  },
+  updated() {
+    // console.log('updated');
+    // const dom=document.querySelector('#pp');
+    // console.log(dom.innerHTML);
+  },
+  beforeDestroy() {
+    console.log('beforeDestroy');
+    console.log(this.message);
+  },
+}
+</script>
+
+<style lang="less">
+.test-container {
+  background-color: pink;
+  height: 200px;
+}
+
+</style>
