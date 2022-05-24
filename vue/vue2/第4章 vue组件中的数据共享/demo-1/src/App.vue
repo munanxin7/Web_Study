8f3da3a1c8e22e6eeeb0f2815934f52ae151af0a
privet <810336311@qq.com>
2022-05-24 16:00:41 +0800

    vue组件基础

---
+++ vue/vue2/第4章 vue组件中的数据共享/demo-1/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr />
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <!-- 以标签形式，使用注册好的组件 -->
+      <Left></Left>
+      <Right></Right>
+    </div>
+  </div>
+</template>
+
+<script>
+  // 导入需要使用的 .vue 组件
+  import Left from '@/components/Left.vue'
+  import Right from '@/components/Right.vue'
+export default {
+  // 注册组件
+  components: {
+    Left,
+    Right
+  }
+}
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
---
+++ vue/vue2/第4章 vue组件中的数据共享/demo-1/src/components/Count.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h5>Count 组件</h5>
+    <p>count 的值是 {{ count }}</p>
+    <button @click="count += 1">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // props 是自定义属性，允许使用者通过自定义属性，为当前组件指定初始值
+  // props 中的数据可以直接在模板结构中使用
+  // 注：只读
+  // props:['init'],
+  props: {
+    init: {
+      // 设置默认值为0
+      default:0,
+      // 设置格式为数字
+      type:Number,
+      // 必填项校验
+      required:true
+    }
+  },
+  data(){
+    return {
+      count:this.init
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
