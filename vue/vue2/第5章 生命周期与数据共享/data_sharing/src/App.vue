a0e65a670ff561eaf797639ae3bcd857b9c3e57e
privet <810336311@qq.com>
2022-05-26 16:12:23 +0800

    数据传递

---
+++ vue/vue2/第5章 生命周期与数据共享/data_sharing/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app-container">
+    <h1>APP根组件 --- {{ countFromSon }}</h1>
+    <hr />
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <Left :msg="message" :user="userinfo"></Left>
+      <Right @numchange="getNewCount"></Right>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from "@/components/Left.vue";
+import Right from "@/components/Right.vue";
+
+export default {
+  data() {
+    return {
+      message: "hello vue",
+      userinfo: {
+        name: "privet",
+        age: 18,
+      },
+      // 定义接收来自子组件传递的数据
+      countFromSon: 0,
+    };
+  },
+  methods: {
+    getNewCount(val) {
+      this.countFromSon = val;
+    },
+  },
+  components: {
+    Left,
+    Right,
+  },
+};
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
---
+++ vue/vue2/第5章 生命周期与数据共享/data_sharing/src/components/Left.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="left-container">
+    <h3>Left 组件</h3>
+    <p>msg 的值是：{{ msg }}</p>
+    <p>user 的值是：{{ user }}</p>
+    <hr />
+    <button @click="send">发送给Right</button>
+  </div>
+</template>
+
+<script>
+// 导入 eventBus.js 模块
+import bus from "./eventBus.js";
+export default {
+  props: ["msg", "user"],
+  data() {
+    return {
+      str: "床前明月光",
+    };
+  },
+  methods: {
+    send() {
+      // 通过 eventBus 发送数据
+      bus.$emit("share", this.str);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.left-container {
+  padding: 0 20px 20px;
+  background-color: orange;
+  min-height: 250px;
+  flex: 1;
+}
+</style>
